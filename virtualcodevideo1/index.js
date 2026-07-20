5f87dd16f22e522799c3a5f6ba0441b4904ec31c
Ashishsahu0107 <ashishsahu01072005@gmail.com>
2026-07-20 15:06:17 +0530

    Day 2 backend

---
+++ virtualcodevideo1/index.js
@@ -0,0 +1,19 @@
+// import http from "http";
+
+// const Port = 3000;
+
+// const server = http.createServer((req, res) => {
+//     if(req.url=="/")
+//         res.end("Welcome to home page");
+//     else if (req.url == "/contact")
+//         res.end("Welcome to contact page");
+//     else if (req.url == "/about")
+//         res.end("Welcome to about page");
+//     else if (req.url == "/skills")
+//         res.end("Welcome to skills page");
+// });
+
+// server.listen(Port, () => { 
+//     console.log("server is started");
+// });
+
--- virtualcodevideo1/server.js
+++ virtualcodevideo1/server.js
@@ -1,10 +1,74 @@
-import http from "http";
+import express from 'express';
+const app = express();
+const port = 3000;
 
-const Port = 3000;
+app.use(express.json())
 
-const server = http.createServer((req, res) => {
-    res.end("Hii dsajkfh first server");
+
+const users = [
+  {
+    "id": 1,
+    "name": "John Doe",
+    "email": "john@example.com",
+    "age": 25,
+    "city": "New York"
+  },
+  {
+    "id": 2,
+    "name": "Jane Smith",
+    "email": "jane@example.com",
+    "age": 28,
+    "city": "London"
+  },
+  {
+    "id": 3,
+    "name": "Michael Johnson",
+    "email": "michael@example.com",
+    "age": 30,
+    "city": "Toronto"
+  },
+  {
+    "id": 4,
+    "name": "Emily Davis",
+    "email": "emily@example.com",
+    "age": 22,
+    "city": "Sydney"
+  },
+  {
+    "id": 5,
+    "name": "David Wilson",
+    "email": "david@example.com",
+    "age": 35,
+    "city": "Berlin"
+  }
+]
+
+
+// app.get('/', (req, res) => {
+//     res.json({name:"Ashish",age:20});
+// });
+
+// app.post("/", (req, res) => {
+//     let aaa = req.body;
+//     console.log(aaa)
+//     res.send("Hello")
+// })
+
+
+
+app.get("/user/:id", (req, res) => { 
+    let id = req.params.id
+
+    let existingUser = users.find((user) => (user.id == id))
+    
+    if (!existingUser) { 
+        res.send("404 not found")
+    }
+    
+    res.json(existingUser)
+})
+
+
+app.listen(port, () => {
+  console.log(`server started port ${port}`);
 });
-
-server.listen(Port);
-
